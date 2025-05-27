64842d0c7c11c5a8ef8e155ce439dccef885f7d8
Wellerson Ferreira de Carvalho <wellersonfc@gmail.com>
2025-05-26 21:20:54 -0300

    base do projeto

---
+++ src/main/java/com/example/apialunos/ApialunosApplication.java
@@ -0,0 +1,13 @@
+package com.example.apialunos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApialunosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApialunosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/apialunos/ApialunosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apialunos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApialunosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
