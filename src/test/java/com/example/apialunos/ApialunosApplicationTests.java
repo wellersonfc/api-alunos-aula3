e1bbadcc12728f7af11a6c865c7209f3302ed91c
Wellerson Ferreira de Carvalho <wellersonfc@gmail.com>
2025-06-07 14:22:06 -0300

    Add user login routes and service

--- src/test/java/com/example/apialunos/ApialunosApplicationTests.java
+++ src/test/java/com/example/apialunos/ApialunosApplicationTests.java
@@ -1,13 +1,27 @@
 package com.example.apialunos;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.example.escola.service.UserService;
+import com.example.escola.model.UserModel;
 
 @SpringBootTest
 class ApialunosApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private UserService userService;
 
+    @Test
+    void contextLoads() {
+        assertThat(userService).isNotNull();
+    }
+
+    @Test
+    void loginFailsWithInvalidCredentials() {
+        assertThat(userService.login("invalid", "invalid").isPresent()).isFalse();
+    }
 }
